f82c9d51db981eeeae4359fac02928c32fb0d49a
kokily <kokily@naver.com>
2021-12-24 06:10:24 +0900

    Production backend config

--- libs/api/client.ts
+++ libs/api/client.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://paysys.kr/api'
+      : 'http://localhost:4000/api',
   withCredentials: true,
 });
 
